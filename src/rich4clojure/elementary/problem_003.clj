4c39ee6e7a584b816bf4064e98e38df7232fc310
Yuanting <yuanting.mao@gmail.com>
2021-11-14 01:04:32 +0000

    getting started with calva and rich 4clojure

--- src/rich4clojure/elementary/problem_003.clj
+++ src/rich4clojure/elementary/problem_003.clj
@@ -16,7 +16,9 @@
   )
 
 (tests
-  __ := (.toUpperCase "hello world"))
+  "HELLO WORLD" := (.toUpperCase "hello world")
+                   ;; => "HELLO WORLD"
+)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/a9e56d5eec902a5e74c4261f3ed3b0ed
